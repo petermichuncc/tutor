0dca16ed8ad2377cc6715867d21415adcd578aa2
petermichuncc <plarrimo@uncc.edu>
2015-05-22 15:10:27 -0400

    I have a demo jobs page working
    It needs to be modified a little but the basic idea is good.

--- client/partshelper.js
+++ client/partshelper.js
@@ -1,6 +1,10 @@
 // Helper for the parts template that feeds it data
 Meteor.subscribe('parts');
-Template.parts.helpers({
+
+
+
+
+Template.job.helpers({
   parts: function() {
     return Parts.find();
    },
@@ -9,6 +13,26 @@ Template.parts.helpers({
      var result = _.values(this.data);
      result.unshift(this.text);
      return result;
-   }
+   },
+   earnedHours: function () {
+        var earnedHoursCalc = (1 * Parts.findOne().cavitation) / Parts.findOne().quantity;
+        return earnedHoursCalc;
+    },
+     incomingCycles: function () {
+        //grab all cycles from today
+        
+        return (100 * Parts.findOne().cavitation);
+    }
+
 });
 console.log ("example is" + Parts.find());
+
+// var partStats = {
+//     workcenterName: Machines.findOne().machinenumber,
+//      partNumber: Parts.findOne().partnumber,
+//      partCycleTime: "23",
+//      partsPlanned: Parts.findOne().quantity,
+//      cavities: Parts.findOne().cavitation,
+//      tech: Parts.findOne().initials,
+//      startTime: start_time
+//  };
