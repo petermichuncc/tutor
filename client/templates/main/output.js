8d36d7c9b2a2208fc8e19e2512ee271743299c1f
petermichuncc <plarrimo@uncc.edu>
2015-09-01 14:58:57 -0400

    I got some of the data for the output page working.
    This will allow the user to see the start and end time for a job.

--- client/templates/main/output.js
+++ client/templates/main/output.js
@@ -14,57 +14,68 @@ planned1: function (){
           //basically I need to know the start time and end time
           //these can be retrieved from the most recently submitted 
             //I need to have the work center associated with the planned
-          var start=Queries.find().fetch().pop().starttime
-          var end=Queries.find().fetch().pop().endtime
-           var pressnum=Queries.find().fetch().pop().press
-          var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+           var start=Queries.find().fetch().pop().starttime
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
              var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
              var ehour= '1'
-           return Planneds.find({timestamp: {$gt: start,$lt: end}, press:pressnum})
+           return Planneds.find({timestamp: {$gt: start,$lt: end}, press:pressnum, pl: ehour}).planneds
           
            },
  actual1: function (){
           //
-           var start=Queries.find().fetch().pop().starttime
-          var end=Queries.find().fetch().pop().endtime
-           var pressnum=Queries.find().fetch().pop().press
-          var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+            var start=Queries.find().fetch().pop().starttime
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
              var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
              var ehour= '1'
-           return Incomingcycles.find({timestamp: {$gt: start,$lt: end}, press:pressnum})
+           return Incomingcycles.find({timestamp: {$gt: start,$lt: end}, press:pressnum, ic:ehour}).incomingcycles
            
           
            },
  earnedhours1: function (){
           //
           var start=Queries.find().fetch().pop().starttime
-          var end=Queries.find().fetch().pop().endtime
-           var pressnum=Queries.find().fetch().pop().press
-          var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
              var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
              var ehour= '1'
-           return Earnedhours.find({timestamp: {$gt: start,$lt: end}, press:pressnum})
+           return Earnedhours.find({timestamp: {$gt: start,$lt: end}, press:pressnum, eh:ehour}).earnedhour
            
-          
            },
 partnumber1: function (){
           //basically I will put the part number that will be stored in the incoming cycles database
           //or I could put the part number associated with the job submitted this hour
-var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+            var start=Queries.find().fetch().pop().starttime
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
              var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
-             var ehour= '1'
-           
-          
+           return Parts.find({timestamp: {$gt: start}, press:pressnum}).fetch().pop().partnumber
       },
  start: function (){
           //This will show when the first job started
-          
-           
+          //I need to find the first part number submitted after this start
+          var start=Queries.find().fetch().pop().starttime
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
+             var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
+           return Parts.find({timestamp: {$gt: start}, press:pressnum}).fetch().pop().timestamp
           //This should just show the the query start
            },
  finish: function (){
           //this will show when the first job ended
-          
+           var start=Queries.find().fetch().pop().starttime
+          var pressnum=Queries.find().fetch().pop().press
+        var start=moment(start).format("YYYY-MM-DD 00:00:00.000")
+        var end=Queries.find().fetch().pop().endtime
+             var end=moment(end).format("YYYY-MM-DD 24:59:99.999")
+             var startime=Parts.find({timestamp: {$gt: start}, press:pressnum}).fetch().pop().timestamp
+           return Hours.find({timestamp: {$gt: start}, press:pressnum}).fetch().pop().timestamp
            
           
            },
